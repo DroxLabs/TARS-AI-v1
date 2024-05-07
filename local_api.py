ba1294cc1354a24f48558b8a4044e22ee4b11333
bahawalkhanbloch <bahawalkhan@folio3.com>
2024-05-07 17:19:50 +0500

    remove upload file from api

--- local_api.py
+++ local_api.py
@@ -29,22 +29,10 @@ client = OpenAI(
     api_key=api_key,
 )
 
-@app.post("/upload/")
-async def upload_files(files: List[UploadFile]):
-	for file in files:
-		with open(file.filename, "wb") as buffer:
-			buffer.write(await file.read())
-
-	file_streams = [open(file.filename, "rb") for file in files]
-	file_names = [file.filename for file in files]
-	file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
-	vector_store_id=STORE_ID, files=file_streams
-	)	
-	return {'upload status': file_batch.status, 'upload_files': file_names}	
 
 
 @app.post("/ask/")
-async def ask_question(question: str):
+async def ask_question(question: str, user_id: str):
 	assistant = client.beta.assistants.update(
 	assistant_id=ASSISTANT_ID,
 	tool_resources={"file_search": {"vector_store_ids": [STORE_ID]}},
@@ -59,12 +47,13 @@ async def ask_question(question: str):
 		role="user",
         content=f"{question}"
 	)
-
 	run = client.beta.threads.runs.create_and_poll(
 		thread_id=thread.id, assistant_id=assistant.id
 	)
 
 	messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
 
+	thread = client.beta.threads.retrieve(thread.id)
+
 	message_content = messages[0].content[0].text
-	return  {"answer": message_content.value}
+	return  {"answer": message_content.value, "thread_id": thread.id}
