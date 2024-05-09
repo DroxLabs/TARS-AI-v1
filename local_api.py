7359d81b14085893cba74a83e4286b7232d0432e
bahawal32 <bahawal30@gmail.com>
2024-05-09 13:26:49 +0500

    add function get coin price by id to assistant

--- local_api.py
+++ local_api.py
@@ -28,35 +28,119 @@ STORE_ID = os.getenv("DROX_LABS_STROE")
 client = OpenAI(
     api_key=api_key,
 )
+tools_list = [{
+    "type": "function",
+    "function":{
+  "name": "get_coin_data_by_id",
+  "description": "query all the coin data of a coin on CoinGecko coin page based on a particular coin id",
+  "parameters": {
+    "type": "object",
+    "properties": {
+      "coin_id": {
+        "type": "string",
+        "description": "unique id for coin"
+      }
+    },
+    "required": [
+      "coin_id"
+    ]
+  }
+}
+}]
 
+assistant = client.beta.assistants.update(
+	assistant_id=ASSISTANT_ID,
+	tool_resources={"file_search": {"vector_store_ids": [STORE_ID]}},
+	tools = tools_list,
+	)
 
+def get_outputs_for_tool_call(tool_call):
+	coin_id = json.loads(tool_call.function.arguments['coin_id'])
+	details = gekko_client.get_coin_data_by_id(coin_id)
+	return {"tool_call_id": tool_call.id,
+		 "output": details
+		 }
 
 @app.post("/ask/")
 async def ask_question(question: str, user_id: str, thread_id: str=None):
-	assistant = client.beta.assistants.update(
-	assistant_id=ASSISTANT_ID,
-	tool_resources={"file_search": {"vector_store_ids": [STORE_ID]}},
-	)
+	
 	# Upload the user provided file to OpenAI
 
 	# Create a thread and attach the file to the message
 	if thread_id:
+		print(thread_id, 'received thread')
 		thread = client.beta.threads.retrieve(thread_id)
 	else:
 		thread = client.beta.threads.create()
+		print(thread.id, 'creating thread')
+
 	
 	message = client.beta.threads.messages.create(
 		thread_id=thread.id,
 		role="user",
-        content=f"{question}"
+        content=f"{question}",
+
 	)
+
 	run = client.beta.threads.runs.create_and_poll(
-		thread_id=thread.id, assistant_id=assistant.id
+		thread_id=thread.id, assistant_id=assistant.id,
+		instructions="Please address yourself as Alex an web3 assistant"
 	)
+	print(run.model_dump_json(indent=4))
 
-	messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
 
-	thread = client.beta.threads.retrieve(thread.id)
+	while True:
+		print('in loop')
+		time.sleep(4)
+		run_status = client.beta.threads.runs.retrieve(
+        thread_id=thread.id,
+        run_id=run.id
+   		)
 
-	message_content = messages[0].content[0].text
-	return  {"answer": message_content.value, "thread_id": thread.id}
+		print(run_status.model_dump_json(indent=4))
+		if run_status.status == 'completed':
+			messages = client.beta.threads.messages.list(thread_id=thread.id)
+			for msg in messages.data:
+				role = msg.role
+				content = msg.content[0].text.value
+				print(f"{role.capitalize()}: {content}")
+				return {'answer':content}
+			break
+		elif run_status.status == 'requires_action':
+			print("Function Calling")
+			required_actions = run_status.required_action.submit_tool_outputs.model_dump()
+			print(required_actions)
+			tool_outputs = []
+			import json
+			for action in required_actions["tool_calls"]:
+				func_name = action['function']['name']
+				arguments = json.loads(action['function']['arguments'])
+				if func_name == "get_coin_data_by_id":
+					output = gekko_client.get_coin_data_by_id(coin_id=arguments['coin_id'])
+					tool_outputs=[
+								{
+								"tool_call_id": action['id'],
+								"output": f'query: {output}'
+								}
+							]
+				
+				
+				else:
+					raise ValueError(f"Unknown function: {func_name}")
+				print("Submitting outputs back to the Assistant...")
+				client.beta.threads.runs.submit_tool_outputs(
+					thread_id=thread.id,
+					run_id=run.id,
+					tool_outputs=tool_outputs
+				)
+			else:
+				print("waiting for outputs...")
+				time.sleep(3)
+
+
+	# messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
+
+	# thread = client.beta.threads.retrieve(thread.id)
+
+	# message_content = messages[0].content[0].text
+	# return  {"answer": message_content.value, "thread_id": thread.id}
