c189e2f7cca04b6eac65aef3bfceb8590cf3fa8a
bahawal32 <bahawal30@gmail.com>
2024-05-22 12:53:51 +0500

    save output and function to return later

--- local_api.py
+++ local_api.py
@@ -107,69 +107,15 @@ async def ask_question(question: str, user_id: str, thread_id: str=None):
         thread_id=thread.id,
         run_id=run.id
    		)
+	output = "NULL"
+	func_name = "NULL"
 
-		if run_status.status == 'completed':
-			messages = client.beta.threads.messages.list(thread_id=thread.id)
-			for msg in messages.data:
-				try:
-					content = msg.content[0].text.value
-					return {'answer':content}
-				except:
-					print(msg.content[0].image_file.file_id)
-					img_file_id = msg.content[0].image_file.file_id
-					image_file = client.files.content(img_file_id)
-					image_data_bytes = image_file.read()
-					headers = {"Content-Type": "image/jpeg"}
 
-					text = msg.content[1].text.value
-					return Response(content=image_data_bytes, headers=headers)
-				
-			break
-		elif run_status.status == 'requires_action':
-			print("Function Calling")
-			required_actions = run_status.required_action.submit_tool_outputs.model_dump()
-			tool_outputs = []
-			import json
-			for action in required_actions["tool_calls"]:
-				func_name = action['function']['name']
-				arguments = json.loads(action['function']['arguments'])
-				print(func_name, arguments)
-				if func_name == "get_coin_data_by_id":
-					output = gekko_client.get_coin_data_by_id(coin_id=arguments['coin_id'])
-					tool_outputs=[
-								{
-								"tool_call_id": action['id'],
-								"output": f'query: {output}'
-								}
-							]
-				
-				elif func_name == "get_coin_historical_data_by_id":
-					output = gekko_client.get_coin_historical_data_by_id(coin_id=arguments['coin_id'], days=arguments['days'], interval=arguments['interval'])
-					tool_outputs=[
-                                {
-                                "tool_call_id": action['id'],
-                                "output": f'query: {output}'
-                                }
-                            ]
-				elif func_name == "get_trend_search":
-					output = gekko_client.get_trend_search()
-					tool_outputs=[
-                                {
-                                "tool_call_id": action['id'],
-                                "output": f'query: {output}'
-                                }
-                            ]
-				else:
-					raise ValueError(f"Unknown function: {func_name}")
-				print("Submitting outputs back to the Assistant...")
-				client.beta.threads.runs.submit_tool_outputs(
-					thread_id=thread.id,
-					run_id=run.id,
-					tool_outputs=tool_outputs
-				)
-			else:
-				print("waiting for outputs...")
-				time.sleep(3)
+	while run_status.status != 'completed':
+		run_status = client.beta.threads.runs.retrieve(
+        thread_id=thread.id,
+        run_id=run.id)
+		try:
 
 
 	# messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
