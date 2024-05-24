21ea67e1c02f227b7c003988491433a601568bdb
bahawal32 <bahawal30@gmail.com>
2024-05-24 15:33:38 +0500

    add flag for chart creatation

--- local_api.py
+++ local_api.py
@@ -104,6 +104,9 @@ async def ask_question(question: str, user_id: str, thread_id: str=None):
    		)
 	output = "NULL"
 	func_name = "NULL"
+	chart = False
+	if any(item in ['chart', 'plot','Chart','Plot'] for item in  question.split(' ')):
+		chart = True
 
 
 	while run_status.status != 'completed':
