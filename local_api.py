464ca87f7480148f287aa6735fdef4c29a0fbdcf
bahawal30 <bahawal30@gmail.com>
2024-06-13 13:40:06 +0500

    get total cost before processing qurey

--- local_api.py
+++ local_api.py
@@ -121,6 +121,10 @@ def calculate_overall_price(input_tokens_used, output_tokens_used, rate_per_mill
 
 @app.post("/ask/")
 async def ask_question(question: str, user_id: str, auth_token: str | None = Header(None), datetime: str | None = Header(None), thread_id: str=None):
+	today = datetime()
+
+	if mongo_store.get_total_cost_for_day(today.year,today.month,today.day) >= 1500:
+		return Response(status_code=200, content="Oops seems like we have reached a limit!")
 
 	if auth_token != AUTH_TOKEN:
 		return Response(status_code=200, content="Invalid Token!")
