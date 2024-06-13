8855d88032483fe328d572f7279628e59d941940
bahawal30 <bahawal30@gmail.com>
2024-06-13 13:51:42 +0500

    fix param passed to get total cost

--- local_api.py
+++ local_api.py
@@ -122,11 +122,11 @@ def calculate_overall_price(input_tokens_used, output_tokens_used, rate_per_mill
 
 @app.post("/ask/")
 async def ask_question(question: str, user_id: str, auth_token: str | None = Header(None), datetime: str | None = Header(None), thread_id: str=None):
-	today = dt()
-
-	if mongo_store.get_total_cost_for_day(today.year,today.month,today.day) >= 1500:
+	total_cost = mongo_store.get_total_cost_for_day(dt.now())
+	if total_cost >= 1500:
 		return Response(status_code=200, content="Oops seems like we have reached a limit!")
-
+	else:
+		print("today's total cost:", total_cost )
 	if auth_token != AUTH_TOKEN:
 		return Response(status_code=200, content="Invalid Token!")
 
